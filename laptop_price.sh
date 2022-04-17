b0384a2341e45baa23bde9461010dac3c48bc9c9
Arnab Chakraborty <arnabchakrabort771@gmail.com>
2022-04-17 20:59:50 +0530

    update

--- laptop_price.sh
+++ laptop_price.sh
@@ -2,10 +2,8 @@
 
 laptop1="Acer Aspire 3 Ryzen 5 Quad Core 3500U"
 laptop2="Acer Aspire 5 Core i5 11th Gen"
-headphone="boAt Bluetooth Headset Fiery Red"
 laptop1_url="https://www.flipkart.com/acer-aspire-3-ryzen-5-quad-core-3500u-8-gb-512-gb-ssd-windows-10-home-a315-23-notebook/p/itm96a9ff5907861"
 laptop2_url="https://www.flipkart.com/acer-aspire-5-core-i5-11th-gen-8-gb-1-tb-hdd-256-gb-ssd-windows-10-home-a515-56-thin-light-laptop/p/itmdf83f58e7a903"
-headphone_url="https://www.flipkart.com/boat-bluetooth-headset/p/itmacb88c86754ff?qH=1d591c38b161e78f"
 
 while true
 do
@@ -15,7 +13,6 @@ do
 
 	[[ $(dunstify -t 10000 -u "normal" "$laptop1" "$laptop1_price" -A Y," Open") = Y ]] && xdg-open "$laptop1_url" &
 	[[ $(dunstify -t 10000 -u "normal" "$laptop2" "$laptop2_price" -A Y," Open") = Y ]] && xdg-open "$laptop2_url" &
-	[[ $(dunstify -t 10000 -u "normal" "$headphone" "$headphone_price" -A Y," Open") = Y ]] && xdg-open "$headphone_url" &
 	paplay /usr/share/sounds/freedesktop/stereo/suspend-error.oga
 
 	sleep 1h
